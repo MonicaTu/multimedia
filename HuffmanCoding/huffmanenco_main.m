2e309b354f332594883ec840d43ba6ffbdc39a4f
Monica Tu <monica.service@gmail.com>
2012-11-22 23:23:01 +0800

    (Huffman, main) Main for adaptived Huffman.

--- HuffmanCoding/huffmanenco_main.m
+++ HuffmanCoding/huffmanenco_main.m
@@ -2,24 +2,22 @@
 %sig = [ 2 1 4 2 1 1 5 4 ];
 %sig_encoded = huffmanenco(sig,dict)
 
-%stream = [12 13 14 15];
-%huffstream = adaptivehuffman(stream,'enc')
-
 %sig = {'a2', 44, 'a3', 55, 'a1'}
 %dict = {'a1',[0]; 'a2',[1,0]; 'a3',[1,1,0]; 44,[1,1,1,0]; 55,[1,1,1,1]}
 %sig_encoded = huffmanenco(sig,dict)
 
 clear;
 
-inStr = input('Input a string: ', 's');
-size = length(inStr);
+size = 100;
+inStr = round(random('unif',1,5,1,size))
+
 m = 1;
 n = 1;
 word(1:size) = 0;
 word_cnt(1:size) = 0;
 
 for i = 1:size
-    loc = findstr(inStr(i), word)
+    loc = findstr(inStr(i), word);
     if isempty(loc);
         word(m) = inStr(i);
         word_cnt(m) = word_cnt(m) + 1;
@@ -35,9 +33,15 @@ for i = 1:(m-1)
     probs(i) = word_cnt(i)/size;
 end
 
+% genetic Huffman
 [dict, avglen] = huffmandict(words, probs);
-sig_encoded = huffmanenco(inStr, dict)
+sig_encoded = huffmanenco(inStr, dict);
 
-src_size = size * 5
+src_size = size * round(log2(size))
 enc_size = length(sig_encoded)
 src_size/enc_size
+
+% adaptive Huffman
+huffstream = adaptivehuffman(uint8(inStr), 'enc');
+adaptive_enc_size = length(huffstream)
+src_size/adaptive_enc_size
